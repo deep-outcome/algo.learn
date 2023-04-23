018b04db391f16ff9090996660a3add1a9e60e1e
JirCep <uzivatel919@seznam.cz>
2023-04-23 17:32:16 +0200

    dividing sort

---
+++ 3.ordering/3.2.ordering by merging/excercises/3.1.5.in_place_sorting_mergesort2/src/dividing_sort.rs
@@ -0,0 +1,119 @@
+/// idea behind this in-place sort labors with
+/// divisibility rules — greater num cannot divide smaller num
+/// so having: a,b,c where c>b ∧ c>a
+/// then when: c*b+a=x ⇒ x%c=a ∧ ⌊x/c⌋=b
+///
+/// when a=b then is obvious why c must be greater than both (a,b) and not equal
+/// to max(a,b)
+/// otherwise % operation raises 0 and / operation result is 1 more greater
+///
+/// Ο(2) space complexity
+/// Ο(2*n) ∊ Ο(n) time complexity, at 1ˢᵗ glanc c (aux operations) is not small
+#[allow(dead_code)]
+pub fn sort(arr: &mut [usize], left: usize, right: usize, ex_end: usize) {
+    let mulcator = arr[right - 1].max(arr[ex_end - 1]) + 1;
+
+    let mut lef_ix = left;
+    let mut rig_ix = right;
+    let mut wri_ix = left;
+
+    while lef_ix < right && rig_ix < ex_end {
+        // modulo operation "retrieves" original num
+        // when it was already overwritten
+        // by proceeding wri_ix
+        //
+        // lef_ix=0, rig_ix=2, wri_ix=0, mulcator=6
+        // +---+---+---+---+
+        // | 3 | 5 | 1 | 4 |
+        // +---+---+---+---+
+        //
+        // after 1ˢᵗ iteration
+        // +---+---+---+---+
+        // | 9 | 5 | 1 | 4 |
+        // +---+---+---+---+
+        // lef_ix=0, rig_ix=3, wri_ix=1 | --> 9%6=3, 3<4
+        //
+        // after 2ⁿᵈ iteration
+        // +---+----+---+---+
+        // | 9 | 23 | 1 | 4 |
+        // +---+----+---+---+
+        let lef_num = arr[lef_ix] % mulcator;
+
+        // ▪ it is not possible to read overwritten nums on right portion
+        // ▪ important is that there is starting gap between wri_ix and rig_ix
+        // ▪ as wri_ix grows with lef_ix increments, gap shortens but this can
+        // result only in wri_ix equal rig_ix at maximum (when only left taken)
+        // ▪ when wri_ix grows with rig_ix increments, gap maintains
+        // ▪ thus there is no need for modulo "retriveval"
+        let rig_num = arr[rig_ix];
+
+        // = maintains stability
+        if lef_num <= rig_num {
+            arr[wri_ix] += lef_num * mulcator;
+            wri_ix += 1;
+            lef_ix += 1;
+        } else {
+            arr[wri_ix] += rig_num * mulcator;
+            wri_ix += 1;
+            rig_ix += 1;
+        }
+    }
+
+    // exhaust left portion if needed
+    while lef_ix < right {
+        arr[wri_ix] += (arr[lef_ix] % mulcator) * mulcator;
+        wri_ix += 1;
+        lef_ix += 1;
+    }
+
+    // exhaust right portion if needed
+    // as proved in core cyclus, when left portion is exhausted
+    // wri_ix == rig_ix
+    while rig_ix < ex_end {
+        // again no modulo
+        arr[rig_ix] *= mulcator;
+        rig_ix += 1;
+    }
+
+    // "extract" current/actual nums
+    wri_ix = left;
+    while wri_ix < ex_end {
+        arr[wri_ix] /= mulcator;
+        wri_ix = wri_ix + 1;
+    }
+}
+
+#[cfg(test)]
+mod tests_of_units {
+    use super::*;
+
+    #[test]
+    fn basic_test() {
+        let mut arr = [3, 4, 1, 2];
+        let mut criterion = arr.clone();
+        criterion.sort();
+
+        sort(&mut arr, 0, 2, 4);
+        assert_eq!(criterion, arr);
+    }
+
+    #[test]
+    fn basic_test2() {
+        let mut arr = [1, 2, 3, 4, 5, 6];
+        let mut criterion = arr.clone();
+        criterion.sort();
+
+        sort(&mut arr, 0, 3, 6);
+        assert_eq!(criterion, arr);
+    }
+
+    #[test]
+    fn basic_test3() {
+        let mut arr = [1, 3, 5, 7, 2, 4, 6, 8];
+        let mut criterion = arr.clone();
+        criterion.sort();
+
+        sort(&mut arr, 0, 4, 8);
+        assert_eq!(criterion, arr);
+    }
+}
--- 3.ordering/3.2.ordering by merging/excercises/3.1.5.in_place_sorting_mergesort2/src/lib.rs
+++ 3.ordering/3.2.ordering by merging/excercises/3.1.5.in_place_sorting_mergesort2/src/lib.rs
@@ -1,13 +1,4 @@
+mod dividing_sort;
 mod in_place_right_shifting;
 mod in_place_shifting;
 mod shellsort;
-
-// 9, 8, 3, 7, 5, 6, 4, 1
-
-// 1, 3, 5, 4, 7, 6, 8, 9
-// 1, 3, 5, 4, 7, 6, 8, 9
-
-// 8, 9, 3, 7, 5, 6, 1, 4
-// 1, 9, 3, 7, 5, 6, 8, 4
-// 1, 4, 3, 6, 5, 7, 8, 9
-//

600ef93163ef59d5117d8ccb3328a9551b029627
JirCep <uzivatel919@seznam.cz>
2023-07-17 07:27:30 +0200

    no-buckets bucketsort memory optimization
    E.g. `size_of::<Option<u8>>() = 2` while `size_of::<* const u8>() = size_of::<usize>()`.

--- 3.ordering/3.4 distribution ordering algorithms/exercises/3.5.2.no_buckets_buckersort/src/lib.rs
+++ 3.ordering/3.4 distribution ordering algorithms/exercises/3.5.2.no_buckets_buckersort/src/lib.rs
@@ -1,5 +1,4 @@
 use std::fmt::Display;
-use std::ptr;
 
 trait KeyProvider {
     fn key(&self) -> usize;
@@ -12,9 +11,9 @@ impl KeyProvider for u32 {
 }
 
 #[allow(dead_code)]
-fn sort<T>(slc: &mut [&T])
+fn sort<T>(slc: &mut [T])
 where
-    T: KeyProvider + Display,
+    T: KeyProvider + Display + Clone,
 {
     if slc.len() < 2 {
         return;
@@ -23,18 +22,18 @@ where
     sort_unccked(slc)
 }
 
-fn sort_unccked<T>(slc: &mut [&T])
+fn sort_unccked<T>(slc: &mut [T])
 where
-    T: KeyProvider + Display,
+    T: KeyProvider + Display + Clone,
 {
     // support for up to 5 same keys
     const KEYS_LEN: usize = 1_000_001 * 5;
-    let mut cask = Vec::<*const T>::with_capacity(KEYS_LEN);
+    let mut cask = Vec::<Option<T>>::with_capacity(KEYS_LEN);
 
     unsafe { cask.set_len(KEYS_LEN) };
 
     for i in 0..KEYS_LEN {
-        cask[i] = ptr::null();
+        cask[i] = None;
     }
 
     'a: for t in slc.iter() {
@@ -43,9 +42,9 @@ where
         key += key * 4;
 
         for _ in 1..=5 {
-            let val = cask[key];
-            if val.is_null() {
-                cask[key] = *t;
+            let val = &cask[key];
+            if val.is_none() {
+                cask[key] = Some(t.clone());
                 continue 'a;
             }
 
@@ -59,10 +58,8 @@ where
     let mut ix = 0;
 
     for x in cask {
-        if x.is_null() {
-            continue;
-        } else {
-            slc[ix] = unsafe { x.as_ref() }.unwrap();
+        if let Some(y) = x {
+            slc[ix] = y;
 
             ix = ix + 1;
             if ix == slc_len {
@@ -78,61 +75,49 @@ mod tests_of_units {
 
     #[test]
     fn basic_test() {
-        let vals = [9, 5, 3, 2, 0];
+        let mut vals = [9, 5, 3, 2, 0];
 
-        let iter = vals.iter();
-        let mut criterion: Vec<&u32> = iter.clone().collect();
+        let mut criterion = vals.clone();
         criterion.sort();
 
-        let mut vals: Vec<&u32> = iter.collect();
         sort_unccked(&mut vals);
-
         assert_eq!(criterion, vals);
     }
 
     #[test]
     fn repetition_test() {
-        let vals = [9, 5, 3, 5, 2, 5, 0, 5, 5, 3, 4, 4, 6, 4, 4, 7, 8, 4, 9];
+        let mut vals = [9, 5, 3, 5, 2, 5, 0, 5, 5, 3, 4, 4, 6, 4, 4, 7, 8, 4, 9];
 
-        let iter = vals.iter();
-        let mut criterion: Vec<&u32> = iter.clone().collect();
+        let mut criterion = vals.clone();
         criterion.sort();
 
-        let mut vals: Vec<&u32> = iter.collect();
         sort_unccked(&mut vals);
-
         assert_eq!(criterion, vals);
     }
 
     #[test]
     #[should_panic(expected = "Maximum repetition count exceeded. Item `5`.")]
     fn too_much_repetition_test() {
-        let vals = [9, 5, 3, 5, 2, 5, 0, 5, 5, 5];
+        let mut vals = [9, 5, 3, 5, 2, 5, 0, 5, 5, 5];
 
-        let iter = vals.iter();
-        let mut criterion: Vec<&u32> = iter.clone().collect();
+        let mut criterion = vals.clone();
         criterion.sort();
 
-        let mut vals: Vec<&u32> = iter.collect();
         sort_unccked(&mut vals);
-
         assert_eq!(criterion, vals);
     }
 
     #[test]
     fn heavy_test() {
-        let vals = [
+        let mut vals = [
             9, 5, 3, 5, 2, 5, 0, 5, 5, 3, 4, 4, 6, 4, 4, 7, 8, 4, 9, 1, 1, 3, 3, 1_000_000, 1000,
             1000, 9, 9,
         ];
 
-        let iter = vals.iter();
-        let mut criterion: Vec<&u32> = iter.clone().collect();
+        let mut criterion = vals.clone();
         criterion.sort();
 
-        let mut vals: Vec<&u32> = iter.collect();
         sort_unccked(&mut vals);
-
         assert_eq!(criterion, vals);
     }
 }
